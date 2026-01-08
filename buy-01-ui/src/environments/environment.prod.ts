79a926af908deeabb91bd4dea2b031d55bdc065e
oafilali <medafilali@gmail.com>
2026-01-08 13:56:27 +0200

    Fix: Update production API URLs to new infrastructure (13.61.234.232)

--- buy-01-ui/src/environments/environment.prod.ts
+++ buy-01-ui/src/environments/environment.prod.ts
@@ -2,11 +2,11 @@
 // This is the PRODUCTION file for Docker builds
 export const environment = {
   production: true,
-  apiUrl: 'http://51.21.198.139:8080/api',
-  apiGatewayUrl: 'http://51.21.198.139:8080',
-  authUrl: 'http://51.21.198.139:8080/api/auth',
-  usersUrl: 'http://51.21.198.139:8080/api/users',
-  productsUrl: 'http://51.21.198.139:8080/api/products',
-  mediaUrl: 'http://51.21.198.139:8080/api/media',
+  apiUrl: 'http://13.61.234.232:8080/api',
+  apiGatewayUrl: 'http://13.61.234.232:8080',
+  authUrl: 'http://13.61.234.232:8080/api/auth',
+  usersUrl: 'http://13.61.234.232:8080/api/users',
+  productsUrl: 'http://13.61.234.232:8080/api/products',
+  mediaUrl: 'http://13.61.234.232:8080/api/media',
   enableDebugLogging: false,
 };
